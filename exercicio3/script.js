54ff1d55adff1fcad2293d2c630807256a0a9002
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-06-29 19:56:41 -0300

    Delete exercicio3 directory

--- exercicio3/script.js
+++
@@ -1,8 +0,0 @@
-// Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
-
-let contadora = prompt("Digite um número maior do que 0 para fazermos uma contagem regressiva");
-
-while (contadora > -1){
-    alert(contadora);
-    contadora--;
-}
