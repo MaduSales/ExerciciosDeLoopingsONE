a6aef2b5c1acd1595b28900903f438946cf85c0f
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-06-29 19:41:46 -0300

    Add files via upload

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+// Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+
+alert('Vamos iniciar uma conatgem');
+
+let contadora = 1;
+
+while (contadora < 11){
+    alert(contadora);
+    contadora++;
+}
---
+++ exercicio2/script.js
@@ -0,0 +1,10 @@
+// Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+
+alert('Agora vamos decrementar!');
+
+let contadora = 10;
+
+while (contadora > 0){
+    alert(contadora);
+    contadora--;
+}
